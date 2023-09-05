f361fa9d0edcba6e371aff56a84bb6be1c99145e
DanCavi <dante.cavieres@hotmail.com>
2023-09-05 12:36:10 -0300

    first commit

---
+++ controller/controller.conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$direcIp = "localhost";
+$name = "root";
+$pass = "sisep+123";
+$db_nombre = "test";
+
+$conexion = mysqli_connect($direcIp, $name, $pass, $db_nombre);
+
+if (!$conexion) {
+    echo "Falló";
+}
---
+++ controller/controller.login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+include "./controller.conexion.php";
+
+if (isset($_POST['email']) && isset($_POST['passw'])) {
+
+    function validar($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+
+$email = validar($_POST['email']);
+$passw = validar($_POST['passw']);
+
+if (empty($email) || empty($passw)) {
+    header("Location: ./../signin.php?error=Llene todos los campos");
+    exit();
+} else {
+    $query = "Select * from usuarios where email = '$email' and password = '$passw'";
+    $rolQuery = "Select rolName from roles inner join usuariosrole where rolId = id and id = 1";
+
+    $resultado = mysqli_query($conexion, $query);
+    $resRol = mysqli_query($conexion, $rolQuery);
+
+    if (mysqli_num_rows($resultado) != 1) {
+        header("Location: ./../signin.php?error=Usuario incorrecto");
+        exit();
+    } else {
+        $columna = mysqli_fetch_assoc($resultado);
+        $rolArr = mysqli_fetch_assoc($resRol);
+        
+        $_SESSION['email'] = $columna['email'];
+        $_SESSION['nombre'] = $columna['name'];
+        $_SESSION['id'] = $columna['id'];
+        $_SESSION['rol'] = $rolArr['rolName'];
+        header("Location: ./../index.php");
+        exit();
+
+    }
+
+}
+
+}
