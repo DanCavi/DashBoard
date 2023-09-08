06911395a46238e26e46c3a65a8b11b198abadda
DanCavi <dante.cavieres@hotmail.com>
2023-09-08 16:53:20 -0300

    order hierarchy js

--- lib/libreria-js.php
+++ lib/libreria-js.php
@@ -3,12 +3,12 @@
 ?>
 
 <script src="<?php echo web; ?>js/jquery-3.4.1.min.js"></script>
-<script src="<?php echo web; ?>js/main.js"></script>
+<script src="<?php echo web; ?>lib/waypoints/waypoints.min.js"></script>
 <script src="<?php echo web; ?>js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo web; ?>lib/chart/chart.min.js"></script>
 <script src="<?php echo web; ?>lib/easing/easing.min.js"></script>
-<script src="<?php echo web; ?>lib/waypoints/waypoints.min.js"></script>
 <script src="<?php echo web; ?>lib/owlcarousel/owl.carousel.min.js"></script>
 <script src="<?php echo web; ?>lib/tempusdominus/js/moment.min.js"></script>
 <script src="<?php echo web; ?>lib/tempusdominus/js/moment-timezone.min.js"></script>
 <script src="<?php echo web; ?>lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?php echo web; ?>js/main.js"></script>
